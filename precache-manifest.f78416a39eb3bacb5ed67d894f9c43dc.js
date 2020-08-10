12b7beea450365dede80dbcb7b390e5183895ac7
Eugene Gorbachev <eugeneg@cqg.com>
2020-08-10 11:46:22 +0300

    Updates

---
+++ precache-manifest.f78416a39eb3bacb5ed67d894f9c43dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82fd11f42045db06e52e53e0b036fda7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6303eaec1bb63d2e73a6",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "dbc6db5c35e17c548e96",
+    "url": "/static/css/main.6c1fd8eb.chunk.css"
+  },
+  {
+    "revision": "6303eaec1bb63d2e73a6",
+    "url": "/static/js/2.d23d15f1.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/static/js/2.d23d15f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbc6db5c35e17c548e96",
+    "url": "/static/js/main.79c9d89e.chunk.js"
+  },
+  {
+    "revision": "4b2b11acf5a730cec34e",
+    "url": "/static/js/runtime-main.fb25291d.js"
+  },
+  {
+    "revision": "4c35d6cfa3172d69e67d0d8699ac00d9",
+    "url": "/static/media/pencil.4c35d6cf.svg"
+  }
+]);
