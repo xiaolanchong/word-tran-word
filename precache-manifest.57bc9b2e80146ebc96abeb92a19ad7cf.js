491f80f47d8028c4206b4ef9f1a1d88ea0d9cd20
Eugene Gorbachev <eugeneg@cqg.com>
2020-08-12 18:31:43 +0300

    Updates

---
+++ precache-manifest.57bc9b2e80146ebc96abeb92a19ad7cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bda971308e63026ff548b27d08f3447",
+    "url": "/word-tran-word/index.html"
+  },
+  {
+    "revision": "713769322d48088213b1",
+    "url": "/word-tran-word/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "fbd99fbc738c09475ed7",
+    "url": "/word-tran-word/static/css/main.6c1fd8eb.chunk.css"
+  },
+  {
+    "revision": "713769322d48088213b1",
+    "url": "/word-tran-word/static/js/2.d97f3a80.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/word-tran-word/static/js/2.d97f3a80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbd99fbc738c09475ed7",
+    "url": "/word-tran-word/static/js/main.616e4315.chunk.js"
+  },
+  {
+    "revision": "0c5e3db3a4098179a5a1",
+    "url": "/word-tran-word/static/js/runtime-main.d53a816d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "http://localhost:3000/precache-manifest.7b933215dab50911855893fdc14bd091.js"
+  "/word-tran-word/precache-manifest.57bc9b2e80146ebc96abeb92a19ad7cf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://localhost:3000/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/word-tran-word/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
